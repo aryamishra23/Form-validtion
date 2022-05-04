49569e6e8622178782dfcd320558fc9cda87c9e7
aryamishra23 <103114935+aryamishra23@users.noreply.github.com>
2022-05-04 13:08:13 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserFormComponent } from './components/user-form/user-form.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component:UserFormComponent
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
